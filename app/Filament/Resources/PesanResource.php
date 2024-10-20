d9bbb75f29c1d91a22eb4bc0d4ff9f564d094933
upi <upiwira39@gmail.com>
2024-10-20 20:06:33 +0800

    penambahan crud

---
+++ app/Filament/Resources/PesanResource.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PesanResource\Pages;
+use App\Filament\Resources\PesanResource\RelationManagers;
+use App\Models\Pesan;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PesanResource extends Resource
+{
+    protected static ?string $model = Pesan::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make(' nama_pesanan')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('jumlah_Pesanan')
+                    ->required()
+                    ->numeric(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make(' nama_pesanan')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('jumlah_Pesanan')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPesans::route('/'),
+            'create' => Pages\CreatePesan::route('/create'),
+            'edit' => Pages\EditPesan::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Models/pesan.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class pesan extends Model
+{
+    
+    protected $fillable =['nama_pesanan', 'jumlah_pesanan'];
+}
