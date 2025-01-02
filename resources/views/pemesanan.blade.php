64e2e4d16b878a91a48c94558a5359ded5f3386e
upi <upiwira39@gmail.com>
2025-01-02 22:02:36 +0800

    ,

--- resources/views/pemesanan.blade.php
+++ resources/views/pemesanan.blade.php
@@ -5,15 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/pemesanan.css">
     <title>AQUANTER Pemesanan</title>
+    
 </head>
 <body>
     <div class="header">
         <h1>AQUANTER</h1>
     </div>
-
     <div class="container">
         <h2>Galon yang Tersedia</h2>
 
+        <!-- Row 1: 4 Products -->
         <div class="product">
             <!-- Baris pertama dengan 4 produk -->
             <div class="item">
@@ -21,10 +22,17 @@
                 <img src="img/le_minerale.png" alt="Mineral">
                 <p>Rp 5.000</p>
             </div>
+            
             <div class="item">
                 <p>LE MINERALE (15 L)</p>
                 <img src="img/le_minerale.png" alt="Mineral">
                 <p>Rp 5.000</p>
+            </div>
+
+            <div class="item">
+                <p>CLEO (19 L)</p>
+                <img src="img/Screenshot-2023-02-07-at-7.43.48-AM.png" alt="Cleo">
+                <p>Rp 10.000</p>
             </div>
             <div class="item">
                 <p>CLEO (19 L)</p>
@@ -37,26 +45,25 @@
                 <p>Rp 10.000</p>
             </div>
 
-            <!-- Baris kedua dengan 4 produk -->
-            <div class="item">
-                <p>CLEO (19 L)</p>
-                <img src="img/Screenshot-2023-02-07-at-7.43.48-AM.png" alt="Cleo">
-                <p>Rp 10.000</p>
-            </div>
+
             <div class="item">
                 <p>AQUA (19 L)</p>
                 <img src="img/aqua.jpg" alt="AQUA">
                 <p>Rp 9.000</p>
             </div>
             <div class="item">
-                <p>AQUA (19 L)</p>
-                <img src="img/aqua.jpg" alt="AQUA">
-                <p>Rp 9.000</p>
+                <p>Sumber Air (19 L)</p>
+                <img src="img/aqua.jpg" alt="Sumber Air">
+                <p>Rp 8.000</p>
             </div>
+        </div>
+
+        <!-- Row 2: 1 Product -->
+        <div class="product single">
             <div class="item">
-                <p>AQUA (19 L)</p>
-                <img src="img/aqua.jpg" alt="AQUA">
-                <p>Rp 9.000</p>
+                <p>AQUA (15 L)</p>
+                <img src="img/aqua.jpg" alt="Alami">
+                <p>Rp 6.000</p>
             </div>
 
             <!-- Baris ketiga dengan 1 produk -->
@@ -67,7 +74,10 @@
             </div>
         </div>
 
-        <button><a href="/admin/pesans">Pesan</a></button>
+            <!-- Add any other necessary fields here -->
+
+            <button><a href="/admin/pesans">Pesan</a></button>
+        </form>
     </div>
     <script src="script/pemesanan.js"></script>
 </body>
